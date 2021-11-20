d3f4fcd1233a4c73e8eec8516e1e2140820e78c2
Yar <1l1l1arr1l1l1@gmail.com>
2021-11-20 11:15:25 +0300

    1

---
+++ script.js
@@ -0,0 +1,13 @@
+function simpleMultiplication(number) {
+  let result = 0;
+  if (number % 2 === 0) {
+  result += number * 8
+  } else {
+  result += number * 9
+  }
+  
+  
+  
+  return result
+  }
+  simpleMultiplication(2)
