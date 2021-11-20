4d435663bbad57de6f368b65e2766a9ac1560fd7
Yar <1l1l1arr1l1l1@gmail.com>
2021-11-20 11:22:52 +0300

    2

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-function simpleMultiplication(number) {
-  let result = 0;
-  if (number % 2 === 0) {
-  result += number * 8
-  } else {
-  result += number * 9
-  }
+// function simpleMultiplication(number) {
+//   let result = 0;
+//   if (number % 2 === 0) {
+//   result += number * 8
+//   } else {
+//   result += number * 9
+//   }
   
   
   
-  return result
-  }
-  simpleMultiplication(2)
+//   return result
+//   }
+//   simpleMultiplication(2)
+let result = 0;
+
+result = laLigaGoals + copaDelReyGoals + championsLeagueGoals
+
+if (laLigaGoals + copaDelReyGoals + championsLeagueGoals === undefined) {
+result = 0;
+}
+
+return result
