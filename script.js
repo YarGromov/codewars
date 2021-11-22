b7a27ef12e92950d217c8485fc0306b531038422
Yar <1l1l1arr1l1l1@gmail.com>
2021-11-22 12:40:46 +0300

    41

--- script.js
+++ script.js
@@ -482,25 +482,39 @@
 // if (s[i] == '!') {
 // continue;
 // } else if (s[i] !== '!') {
-// result += s[i]
-// }
-// }
-// return result;
-// }
-// removeExclamationMarks("Hello World!!!!!!")
-function basicOp(operation, value1, value2) {
-  let result = 0;
-  if (operation == '*') {
-  result = value1 * value2
-  } else if (operation == '+' ) {
-  result = value1 + value2
-  } else if (operation == '-' ) {
-  result = value1 - value2
-  } else if (operation == '/' ) {
-  result = value1 / value2
+// // result += s[i]
+// // }
+// // }
+// // return result;
+// // }
+// // removeExclamationMarks("Hello World!!!!!!")
+// function basicOp(operation, value1, value2) {
+//   let result = 0;
+//   if (operation == '*') {
+//   result = value1 * value2
+//   } else if (operation == '+' ) {
+//   result = value1 + value2
+//   } else if (operation == '-' ) {
+//   result = value1 - value2
+//   } else if (operation == '/' ) {
+//   result = value1 / value2
+//   }
+  
+  
+  
+//   return result;
+  
+function areYouPlayingBanjo(name) {
+  let result = '';
+  if (name[0] === 'R' || name[0] === 'r') {
+  result = `${name} plays banjo`
+  } else {
+  result = `${name} does not play banjo`
   }
   
   
   
-  return result;
   
+    return result;
+  }
+  areYouPlayingBanjo("aul")
