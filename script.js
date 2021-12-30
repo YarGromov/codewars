643a86bcfd66d2d4f825e70adf6238a891f92ddd
Yar <1l1l1arr1l1l1@gmail.com>
2021-12-30 13:20:21 +0300

    56

--- script.js
+++ script.js
@@ -5,9 +5,7 @@
 //   } else {
 //   result += number * 9
 //   }
-  
-  
-  
+
 //   return result
 //   }
 // //   simpleMultiplication(2)
@@ -22,48 +20,39 @@
 // return result
 // function isDivisible(n, x, y) {
 //   let result = true;
-  
+
 //   if (Number.isInteger(n / x) && Number.isInteger(n / y)) {
 //   result = true;
 //   } else {
 //   result = false
 //   }
-  
-  
-  
-  
+
 //   return result
 //   }
 //   isDivisible(3, 3, 4)
 // function doubleInteger(i) {
 //   i = i * 2
-  
-  
+
 //     return i;
 //   }
 //   doubleInteger(3)
 // // function doubleInteger(a, b) {
 // //   let result = 0;
-  
+
 // //   result += a * b
-  
-  
-  
-  
+
 // //   return result
 // //   }
 // //   doubleInteger(3, 4)
 // function makeNegative(num) {
 //   let result = 0;
-  
+
 //   if (num > 0) {
 //   result = -num
 //   } else {
 //   result = num
 //   }
-  
-  
-  
+
 //   return result
 //   }
 // //   makeNegative(-5)
@@ -73,38 +62,33 @@
 // }
 // function move (position, roll) {
 //   let result = 0;
-  
+
 //   result = position + (roll * 2)
-  
-  
-  
+
 //   return result;
 //   }
 //   move (2, 5)
 // function even_or_odd(number) {
 //   let result = '';
-  
+
 //   if (number % 2 == 0) {
 //   result = 'Even';
 //   } else {
 //   result = 'Odd'
 //   }
-  
-  
-  
+
 //   return result;
 //   }
 //   even_or_odd(21)
 // function opposite(number) {
 //   let result = -number;
-  
-  
+
 //   return result;
 //   }
 // //   opposite(-3)
 // function lovefunc(flower1, flower2){
 //   let result;
-  
+
 //   if (flower1 % 2 === 0 && flower2 % 2 !== 0 ) {
 //   result = true;
 //   } else if (flower2 % 2 === 0 && flower1 % 2 !== 0 ) {
@@ -112,27 +96,24 @@
 //   } else {
 //   result = false;
 //   }
-  
-  
-  
+
 //   return result;
 //   }
 //   lovefunc(3, 3)
 // function litres(time) {
-//   let result; 
-  
+//   let result;
+
 //   let count = time * 0.5;
-  
+
 //   result = Math.floor(count)
-  
-  
+
 //   return result;
 //   }
 //   litres(11.8)
 // function rentalCarCost(d) {
 //   let result = 0;
 //   let price = 40;
-  
+
 //   if (d < 3) {
 //   result = d * price;
 //   } else if ( d >= 3 && d < 7) {
@@ -140,38 +121,31 @@
 //   } else if ( d >= 7) {
 //   result = (d * price) - 50;
 //   }
-  
-  
-  
-  
+
 //   return result;
 //   }
 // //   rentalCarCost(7)
 // function square(n) {
 //   let result = n ** 2
-  
-  
+
 //   return result;
 //   }
 //   square(3)
 // function numberToString(num) {
 //   let result = num.toString()
-  
-  
+
 //   return result
 //   }
 //   numberToString(3)
 // function between(a, b) {
 //   let result = [];
-  
+
 //   let i = a;
 //   while (i <= b) {
 //   result.push(i);
 //   i++
 //   }
-  
-  
-  
+
 //   return result;
 //   }
 //   between(-1, 5)
@@ -185,68 +159,56 @@
 //   result = 'drink beer'
 //   } else if(old >= 21) {
 //   result = 'drink whisky'
-//   } 
-  
-  
-  
+//   }
+
 //   return result;
 //   };
 // //   peopleWithAgeDrink(13)
 // function isDivideBy(number, a, b) {
 //   let result = true;
-  
+
 //   if (number % a === 0 && number % b === 0) {
 //   result = true;
 //   } else {
 //   result = false;
 //   }
-  
-  
-  
-  
-  
+
 //   return result;
 //   }
 // //   isDivideBy(45, 5, 9)
 // function hoopCount (n) {
 //   let result = '';
-  
+
 //   if (n >= 10) {
 //   result = 'Great, now move on to tricks';
 //   } else {
 //   result = 'Keep at it until you get it';
 //   }
-  
-  
-  
-// //   return result; 
+
+// //   return result;
 // //   }
 // //   hoopCount(10)
 // function findDifference(a, b) {
 //   let result = 0;
 //   let first = 1;
 //   let second = 1;
-  
+
 //   for (let i = 0; i < a.length; i++) {
 //   first *= a[i]
 //   }
-  
+
 //   for (let i = 0; i < b.length; i++) {
 //   second *= b[i]
 //   }
-  
+
 //   result = Math.max(first,second) - Math.min(first,second)
-  
-  
-  
-  
-  
+
 //   return result;
 //   }
 //   findDifference([3, 2, 5], [1, 4, 4])
 // function howMuchILoveYou(nbPetals) {
 //   let result = '';
-  
+
 //   if (nbPetals === 1 || nbPetals === 7) {
 //   result = 'I love you'
 //   } else if (nbPetals === 2) {
@@ -262,9 +224,7 @@
 //   } else if (nbPetals >= 8) {
 //   result = '';
 //   }
-  
-  
-  
+
 //   return result;
 //   }
 //   howMuchILoveYou(460)
@@ -273,12 +233,10 @@
 // let count = 0;
 // let result;
 
-
 // for (let i = 0; i < array.length; i++) {
 //       sum += array[i]
 //       count++
 // }
-
 
 // if (array.length == 0) {
 // result = 0;
@@ -286,91 +244,72 @@
 // result = sum / count;
 // }
 
-
 // return result;
 // }
 // find_average([1, 2, 3, 4])
 // function repeatStr (n, s) {
 //   let result = '';
-  
-//   let i = 0; 
+
+//   let i = 0;
 //   while (i < n) {
 //   result += s
 //   i++
 //   }
-  
-  
-  
-  
-// //   return result; 
+
+// //   return result;
 // //   }
 // //   repeatStr(7, "ha ")
 // function solution(str){
 //   let result = "";
-  
+
 //   for (let i = str.length -1; i >= 0 ; i--) {
 //   result += str[i]
 //   }
-  
-  
-  
+
 //   return result
 // //   }
 // //   solution('Incubator')
 // function sumStr(a,b) {
 //   let result = 0;
-  
+
 //   result = Number(a) + Number(b)
-  
-  
-  
-  
-  
+
 //   return result.toString()
 //   }
 //   sumStr('2', '3')
 // function removeChar(str){
 //   let result = str.slice(1, -1)
- 
- 
+
 //  return result;
 // //  }
 // //  removeChar('Petushka')
 // function findSmallestInt(args) {
 //   let count = [];
-  
-  
-  
+
 //   for (let i = 0; i < args.length; i++) {
 //   count = args.sort(function(a, b) { return a - b; });
 //   }
-  
-  
-  
-  
-//   return count[0]; 
+
+//   return count[0];
 //   }
 // //   findSmallestInt([78,56,232,12,8])
 // function greet(name){
 //   let result = '';
-  
+
 //   result = `Hello, ${name} how are you doing today?`;
-  
-  
-  
+
 //   return result
 //   }
 //   greet('Yaroslav')
 // function boolToWord( bool ){
 //   let result;
-  
+
 //   if (bool == true) {
 //   result = 'Yes'
 //   } else if (bool == false) {
 //   result = 'No'
 //   }
-  
-  
+
 //   return result;
 //   }
 //    boolToWord(false)
@@ -379,25 +318,25 @@
 //   let a = '';
 //   let b = '';
 //   for (let i = 0; i < name.length; i++) {
-  
+
 //   if (i == 0) {
 //   a = name[i].toUpperCase()
 //   }
-  
+
 //   if ( name[i - 1] == ' '){
 //   b = name[i].toUpperCase()
 //   }
-  
+
 //   }
 //   result = `${a}.${b}`
-      
+
 //   return result
 // //   }
 // //   abbrevName("sam harris")
 // function bmi(weight, height) {
 //   let result = '';
 //   let bm = weight / (height ** 2);
-  
+
 //   if (bm <= 18.5 ) {
 //   result = "Underweight";
 //   } else if ( bm <= 25.0 ) {
@@ -407,19 +346,17 @@
 //   } else if ( bm  > 30) {
 //   result = "Obese";
 //   }
-  
-  
-  
+
 //   return result
 // //   }
 // //   bmi(80, 1.80)
 // function updateLight(current) {
 //   let result = '';
-  
+
 //   if (current == 'green') {
 //   result = 'yellow';
 //   } else if (current == 'yellow') {
-//   result = 'red'; 
+//   result = 'red';
 //   } else if (current = 'red') {
 //   result = 'green'
 //   }
@@ -428,19 +365,15 @@
 // //   updateLight("green")
 // const reverseSeq = n => {
 //   let result = [];
-  
+
 //   for (let i = n; i > 0; i--) {
 //   result.push(i)
 //   }
-  
-  
-  
-  
+
 //   return result;
 //   };
 //   reverseSeq(5);
 // function reverseWords(str){
-
 
 //   let result = str.split(' ').reverse().join(' ');
 //   return result;
@@ -459,18 +392,16 @@
 // var min = function(list){
 
 //   let arr = list.sort( (a, b) => a - b );
-  
-  
+
 //   let result = 0;
 //   return arr[0];
 //   }
 //   min([-52, 56, 30, 29, -54, 0, -110]);
-  
-  
+
 //   var max = function(list){
-  
+
 //       let res = list.sort( (a, b) => a - b );
-  
+
 //       return res[res.length -1];
 //   }
 //   max([4,6,2,1,9,63,-134,566])
@@ -499,11 +430,9 @@
 // //   } else if (operation == '/' ) {
 // //   result = value1 / value2
 // //   }
-  
-  
-  
+
 // //   return result;
-  
+
 // function areYouPlayingBanjo(name) {
 //   let result = '';
 //   if (name[0] === 'R' || name[0] === 'r') {
@@ -511,10 +440,7 @@
 //   } else {
 //   result = `${name} does not play banjo`
 //   }
-  
-  
-  
-  
+
 //     return result;
 //   }
 // //   areYouPlayingBanjo("aul")
@@ -529,27 +455,22 @@
 // function multiTable(number) {
 //   let count = 0;
 //   let result = '';
-  
+
 //   for (let i = 1; i < 10; i++) {
 //   result += `${i} * ${number} = ${i * number}\n`;
 //   count++
 //   }
-  
+
 //   if (count == 9) {
 //   result += `${10} * ${number} = ${10 * number}`
 //   }
-  
-  
-  
-  
-//   return result; 
+
+//   return result;
 //   }
 //   multiTable(2)
 // function defineSuit(card) {
 //   let result = '';
-  
-  
-  
+
 //   if ((card[card.length-1]) == '♣') {
 //   result += 'clubs';
 //   } else if ((card[card.length-1]) == '♦') {
@@ -559,30 +480,25 @@
 //   } else if ((card[card.length-1]) == '♠') {
 //   result += 'spades';
 //   }
-  
+
 //   return result;
 //   }
 //   defineSuit("7♠")
 // function sayHello(name) {
 //   let result = '';
-  
+
 //   result = 'Hello, ' + name
-  
-  
-  
-//   return result; 
+
+//   return result;
 //   }
 //   sayHello('Liutenant Uhura')
 // function distinct(a) {
 //   let result = [];
-  
+
 //     result = a.filter(function(item, pos) {
 //       return a.indexOf(item) == pos;
 //   })
-  
-  
-  
-  
+
 //   return result
 //   }
 // //   distinct([1,1,2,2,2,3,4,3,5, 4, 4, 5, 6, 5, 7])
@@ -597,30 +513,26 @@
 //     result += price
 //   }
 
-
 // //   return result;
-// // } 
+// // }
 // // billboard("Simon Eadwulfk")
-// function strCount(str, letter){  
-//   let result = 0; 
-  
+// function strCount(str, letter){
+//   let result = 0;
+
 //     for (let i = 0; i < str.length; i++) {
 //       if (letter == str[i]) {
 //         result++
 //       }
 //     }
-  
-  
-  
+
 //     return result
 //   }
 //   strCount('Hello', 'o')
 // function solution(a, b){
 //   let result = '';
 //   let countA = 0;
-//   let countB = 0; 
-    
-  
+//   let countB = 0;
+
 //     for (let i = 0; i < a.length; i++) {
 //       countA++
 //     }
@@ -631,11 +543,8 @@
 //       result = `${b}${a}${b}`;
 //     } else if (countA < countB) {
 //       result = `${a}${b}${a}`;
-//     } 
-    
-  
-  
-  
+//     }
+
 //   return result;
 //   }
 //   solution('Soon', 'Meeee')
@@ -645,14 +554,13 @@
 // makeUpperCase("hello")
 // function findMultiples(integer, limit) {
 //   let result = [];
-  
+
 //     for (let i = integer; i < limit; i++) {
 //       if (i % integer == 0) {
 //         result.push(i)
 //       }
 //     }
-  
-  
+
 //     return result;
 //   }
 //   findMultiples(4, 27)
@@ -668,18 +576,28 @@
 //   return verb + noun
 // }
 // main('take ', 'item')
-function feast(beast, dish) {
+// function feast(beast, dish) {
+//   let result;
+//     if (beast[0] === dish[0] && beast[beast.length -1] === dish[dish.length -1]) {
+//       result = true;
+//     } else {
+//       result = false
+//     }
+
+//     return result;
+//   }
+//   feast("great blue hero", "garlic naan")
+function hero(bullets, dragons){
   let result;
-    if (beast[0] === dish[0] && beast[beast.length -1] === dish[dish.length -1]) {
-      result = true;
-    } else {
-      result = false
-    }
-  
-  
-  
-  
-  
-    return result;
+    
+  if ((bullets / dragons) >= 2) {
+    result = true;
+  } else {
+    result = false
   }
-  feast("great blue hero", "garlic naan")
+  
+  
+  
+  return result 
+  }
+  hero(1500, 749)
